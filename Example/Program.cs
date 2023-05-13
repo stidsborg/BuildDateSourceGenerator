4c914b156ea293120130f9b3ecaa2006e0a37ae5
stidsborg <stidsborg@cleipnir.net>
2023-05-13 11:53:44 +0200

    Initial commit

---
+++ Example/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace SourceGeneratorExample;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Helper.DeterministicBuild);
+        Console.WriteLine(new DateTime(Helper.BuildTime));
+    }
+}
